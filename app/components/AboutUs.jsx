128d01ccf5057f800f423a7496c65a33353e81af
winnie-webb <71949190+winnie-webb@users.noreply.github.com>
2024-10-28 08:33:06 -0500

    Changed about us content

--- app/components/AboutUs.jsx
+++ app/components/AboutUs.jsx
@@ -7,11 +7,19 @@ function AboutUs() {
       <div className="flex flex-col md:flex-row md:items-center justify-between gap-5">
         <div className="md:flex-[55%]">
           <h2 className="font-bold text-3xl md:text-4xl  md:mb-8">
-          Get to know us
+            Get to know us
           </h2>
           <p className="font-medium text-lg md:leading-8">
-          At Jamaica Eternal Tours, we specialize in uncovering the hidden gems of Jamaica, offering tailored experiences that showcase the island’s beauty, culture, and heritage. From airport transfers to beach escapes and cultural explorations, we provide personalized, expert-guided tours that suit every traveler. Let us take you on an unforgettable journey through Jamaica’s stunning landscapes and iconic attractions, ensuring your comfort and convenience every step of the way.
-</p>          </div>
+            At Shango Tours, we specialize in revealing {"Jamaica's"} hidden
+            gems, offering curated experiences that highlight the island’s
+            natural beauty, rich culture, and vibrant heritage. From airport
+            transfers to beach getaways and cultural immersions, we provide
+            personalized, expertly guided tours for every type of traveler. Let
+            us guide you on an unforgettable journey through {"Jamaica's"}{" "}
+            breathtaking landscapes and iconic landmarks, with your comfort and
+            convenience at the heart of every adventure.
+          </p>{" "}
+        </div>
         <div className="relative w-90 h-72 md:flex-[45%] md:mt-14 md:h-[22rem] ">
           <Image
             alt="Image of Founder with clients from Jamaica Eternal Tours"
@@ -25,14 +33,19 @@ function AboutUs() {
         <div className="md:flex-[50%]">
           <h3 className="font-bold text-3xl md:mb-8 mb-4">Our Mission</h3>
           <p className="font-medium text-base md:leading-7">
-          To empower travelers to explore Jamaica’s finest tours, creating meaningful connections and unforgettable experiences while promoting responsible and sustainable tourism practices.          </p>
+            To empower travelers to explore {"Jamaica's "}finest tours,
+            fostering meaningful connections and creating unforgettable
+            experiences, all while promoting responsible and sustainable
+            tourism.
+          </p>
         </div>
         <div className="md:flex-[50%]">
           <h3 className="font-bold text-3xl md:mb-8 mb-4">Our Vision</h3>
           <p className="font-medium text-base md:leading-7">
-          Our vision is to inspire transformative adventures in Jamaica, fostering appreciation for diverse cultures and contributing to a sustainable, interconnected planet through travel joy.
-
-</p>
+            To inspire transformative adventures in Jamaica, cultivating an
+            appreciation for diverse cultures and contributing to a more
+            sustainable and interconnected world through the joy of travel.
+          </p>
         </div>
       </div>
     </section>
